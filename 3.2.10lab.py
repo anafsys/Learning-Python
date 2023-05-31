8d85bb6bb05769f1a7b82c26718b57f88191defa
Ana <anafsys@gmail.com>
2023-05-31 15:12:28 -0300

    mensagem

---
+++ 3.2.10lab.py
@@ -0,0 +1,19 @@
+# Solicita que o usuário insira uma palavra
+user_word = input("Enter a word: ")
+
+# e atribua-a à variável user_word.
+user_word = user_word.upper()
+
+for letter in user_word:
+    # Preencha o corpo do loop for.
+    if letter == 'A':
+        continue
+    elif letter == 'E':
+        continue
+    elif letter == 'I':
+        continue
+    elif letter == 'O':
+        continue
+    elif letter == 'U':
+        continue
+    print(letter)
